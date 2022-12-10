05f95e227e8a37f62f3a4cc5524e32e4b6944924
crissdot <crissdotdev@gmail.com>
2022-12-09 20:54:47 -0600

    add bcrypt

--- app/models/User.ts
+++ app/models/User.ts
@@ -1,17 +1,29 @@
 import { Sequelize, DataTypes } from 'sequelize';
+import bcrypt from 'bcrypt';
 
 const defineUser = (sequelize: Sequelize) => {
-  return sequelize.define('User', {
+  const User = sequelize.define('User', {
     username: {
       type: DataTypes.STRING,
       unique: true,
       allowNull: false,
     },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
+    passwordHashed: DataTypes.STRING,
+    password: DataTypes.VIRTUAL,
   });
+
+  User.beforeCreate((user, options) => {
+    return new Promise((res, rej) => {
+      if(user.password) {
+        bcrypt.hash(user.password, 10, (error, hash) => {
+          user.passwordHashed = hash;
+          res();
+        });
+      }
+    });
+  });
+
+  return User;
 };
 
 export { defineUser };
