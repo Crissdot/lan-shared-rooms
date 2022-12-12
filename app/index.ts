371b0315104f841b501381add42c3b19b0d1dc93
crissdot <crissdotdev@gmail.com>
2022-12-11 19:16:19 -0600

    add web sockets

--- app/index.ts
+++ app/index.ts
@@ -1,5 +1,6 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
+import { Server } from 'socket.io';
 import { setupSequelize } from './core/sequelize';
 import { routerApi } from './controllers';
 import { logErrors, httpErrorHandler, ormErrorHandler, errorHandler } from './middlewares/errorHandler';
@@ -22,7 +23,7 @@ app.use(httpErrorHandler);
 app.use(ormErrorHandler);
 app.use(errorHandler);
 
-app.listen(port, async () => {
+const server = app.listen(port, async () => {
   try {
     const sequelize = await setupSequelize();
     await sequelize.authenticate();
@@ -33,4 +34,20 @@ app.listen(port, async () => {
     console.error('Unable to connect to the database:', error);
     return;
   }
+});
+
+const io = new Server(server);
+
+let usersCount = 0;
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  usersCount++;
+  io.emit('count_updated', {count: usersCount});
+
+  io.on('disconnect', () => {
+    console.log('User disconnected');
+    usersCount--;
+    io.emit('count_updated', {count: usersCount});
+  });
 });
