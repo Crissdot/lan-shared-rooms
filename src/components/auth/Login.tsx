927f7894238800f5b5b976ab22fdeb64f3000eba
crissdot <crissdotdev@gmail.com>
2022-12-17 12:29:53 -0600

    add types

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -2,17 +2,13 @@ import React from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { useAppDispatch } from '../../hooks/typedRedux';
 import { login } from '../../store/user';
-
-interface ILoginInput {
-  username: string;
-  password: string;
-}
+import { ILoginCredentials } from '../../types/ILoginCredentials';
 
 const Login = () =>  {
   const dispatch = useAppDispatch();
 
-  const { register, handleSubmit } = useForm<ILoginInput>();
-  const onSubmit: SubmitHandler<ILoginInput> = (data) => {
+  const { register, handleSubmit } = useForm<ILoginCredentials>();
+  const onSubmit: SubmitHandler<ILoginCredentials> = (data) => {
     dispatch(login(data));
   }
 
--- src/store/user.ts
+++ src/store/user.ts
@@ -1,22 +1,14 @@
 import axios from 'axios';
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { ILoginCredentials } from '../types/ILoginCredentials';
+import { IUserState } from '../types/IUserState';
 
-interface LoginCredentials {
-  username: string;
-  password: string;
-}
-
-export const login = createAsyncThunk('user/login', async (data: LoginCredentials) => {
+export const login = createAsyncThunk('user/login', async (data: ILoginCredentials) => {
   const response = await axios.post('http://localhost:9999/api/v1/auth/login', data);
   return response.data.data.user;
 });
 
-interface UserState {
-  user: object | null;
-  status: string;
-}
-
-const userInitialState: UserState = {
+const userInitialState: IUserState = {
   user: null,
   status: 'idle',
 }
