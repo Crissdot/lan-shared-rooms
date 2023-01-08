abe6bf6e7e72be8a34b68ff7c55b95087807ff10
crissdot <crissdotdev@gmail.com>
2023-01-08 11:00:35 -0600

    add home tittle

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Main } from './StyledComponents/Main';
+
+const Title = styled.h1`
+  font-size: 2rem;
+  text-align: center;
+`;
 
 const Home = () => {
   return (
-    <h1>Hola Mundo</h1>
+    <Main>
+      <>
+        <Title>LAN SHARED ROOMS</Title>
+      </>
+    </Main>
   );
 }
 
