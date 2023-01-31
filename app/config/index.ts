6eb67299569db99247b909b43e48277db7bc0b1f
crissdot <crissdotdev@gmail.com>
2023-01-30 21:13:35 -0600

    fix backend domain doesn't have the port number

--- app/config/index.ts
+++ app/config/index.ts
@@ -7,5 +7,5 @@ const backendPort = process.env.PORT || 3000;
 export const config = {
   PORT: backendPort,
   FRONTEND_DOMAIN: process.env.FRONTEND_DOMAIN || 'http://localhost:3001',
-  BACKEND_DOMAIN: process.env.BACKEND_DOMAIN || 'http://localhost:' + backendPort,
+  BACKEND_DOMAIN: (process.env.BACKEND_DOMAIN || 'http://localhost') + `:${backendPort}`,
 };
