c8de7eba93e74bf8b88f8b36e3b53eb38337ffb3
crissdot <crissdotdev@gmail.com>
2023-02-02 17:39:04 -0600

    add file saver when click on download button

--- src/components/Room/ChatRoom.tsx
+++ src/components/Room/ChatRoom.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { io } from "socket.io-client";
+import { saveAs } from 'file-saver';
 import { postService } from '../../services/postService';
 import { IFetchedPost, IFilePost } from '../../types/IPost';
 import { ChatInput } from './ChatInput';
@@ -93,6 +94,10 @@ const ChatRoom = () => {
       return <FileItemText>{file.name}</FileItemText>;
     }
 
+    const onClickDownloadHandler = () => {
+      saveAs(file.path, file.name);
+    }
+
     return (
       <FileItemContainer>
         {renderImageIfRequired()}
@@ -102,7 +107,7 @@ const ChatRoom = () => {
             <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
           </svg>
           </FileItemButton>
-          <FileItemButton>
+          <FileItemButton onClick={onClickDownloadHandler}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black">
               <path fillRule="evenodd" d="M12 2.25a.75.75 0 01.75.75v11.69l3.22-3.22a.75.75 0 111.06 1.06l-4.5 4.5a.75.75 0 01-1.06 0l-4.5-4.5a.75.75 0 111.06-1.06l3.22 3.22V3a.75.75 0 01.75-.75zm-9 13.5a.75.75 0 01.75.75v2.25a1.5 1.5 0 001.5 1.5h13.5a1.5 1.5 0 001.5-1.5V16.5a.75.75 0 011.5 0v2.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V16.5a.75.75 0 01.75-.75z" clipRule="evenodd" />
             </svg>
